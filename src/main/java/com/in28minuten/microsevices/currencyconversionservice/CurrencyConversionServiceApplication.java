3b9e82d5f3547bf9ff037dba3112f59b4a3a574a
yanad <yana.danilova1509@gmail.com>
2023-08-31 15:29:15 +0200

    Feing

--- src/main/java/com/in28minuten/microsevices/currencyconversionservice/CurrencyConversionServiceApplication.java
+++ src/main/java/com/in28minuten/microsevices/currencyconversionservice/CurrencyConversionServiceApplication.java
@@ -2,7 +2,9 @@ package com.in28minuten.microsevices.currencyconversionservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
+@EnableFeignClients
 @SpringBootApplication
 public class CurrencyConversionServiceApplication {
 
